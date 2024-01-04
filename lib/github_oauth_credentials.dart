726825cfc4cbb527d3fe45af60211bc4b20f318c
Ken Miyashita <kmiyashita23@gmail.com>
2024-01-04 16:09:41 +0900

    feat: oauth2 works

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'ecbdb1ba2497ba0427b1';
+const githubClientSecret = 'f080491822ce837c9efc5004decf392eaa11ef7d';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
